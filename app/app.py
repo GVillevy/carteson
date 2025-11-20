c264742af843dbf8563a102955f00b8c957d2296
VILLEVY Geoffrey <gvillevy@macbookair-1.home>
2025-11-20 02:02:09 +0100

    changement ui

--- app/app.py
+++ app/app.py
@@ -106,6 +106,6 @@ def famous():
 
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))  # Render fournit le port via cette variable
-    #app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render fournit le port via cette variable
+    app.run(host="0.0.0.0", port=port)
+    #app.run(debug=True)
