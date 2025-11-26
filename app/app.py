57523451ff8f33e40c911f8a315e606195ba3a71
VILLEVY Geoffrey <gvillevy@macbookair-1.home>
2025-11-26 02:08:39 +0100

    update ui

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import csv
 from datetime import date
 import os
+import sqlite3
 
 app = Flask(__name__)
 
@@ -13,21 +14,30 @@ def home():
 
 @app.route("/")
 def pepites():
-    
-    today = date.today() 
-    formatted_date = today.strftime("%d-%m-%Y")
-    formatted_date = "22-11-2025"
+
+    # Connexion au fichier SQLite
+    conn = sqlite3.connect("../db.sqlite")
+    cursor = conn.cursor()
+
+    # Exécution de la requête
+    cursor.execute("SELECT * FROM pepites WHERE running_date = (SELECT MAX(running_date) FROM pepites) ORDER BY score DESC;")
+    rows = cursor.fetchall()  # récupère toutes les lignes
+
+    # Récupérer les noms des colonnes pour construire des dictionnaires
+    columns = [col[0] for col in cursor.description]
+
+    # Préparer les listes
     artistes = []
     difference_days_list = []
 
-    # Lecture CSV
-    with open("../res_trie/res_trie"+formatted_date+".csv", newline="", encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            row["difference_days"] = int(row["difference_days"])
-            difference_days_list.append(row["difference_days"])
-            artistes.append(row)
-            running_date = row["running_date"]
+    for row in rows:
+        row_dict = dict(zip(columns, row))  # transforme la ligne en dict
+        row_dict["difference_days"] = int(row_dict["difference_days"])  # si nécessaire
+        difference_days_list.append(row_dict["difference_days"])
+        artistes.append(row_dict)
+        running_date = row_dict["running_date"]  # tu peux récupérer la dernière date par exemple
+
+    conn.close()
 
     # Calcul moyenne
     moyenne = sum(difference_days_list) / len(difference_days_list)
@@ -55,57 +65,12 @@ def pepites():
     for row in artistes:
         row["color"] = value_to_color(row["difference_days"])
 
+
+
     return render_template("pepites.html", artistes=artistes,running_date=running_date)
 
 
-
-@app.route("/famous")
-def famous():
-
-    today = date.today() 
-    formatted_date = today.strftime("%d-%m-%Y")
-    formatted_date = "14-11-2025"
-    artistes = []
-    difference_days_list = []
-
-    # Lecture CSV
-    with open("../res_famous/res_famous"+formatted_date+".csv", newline="", encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            row["difference_days"] = int(row["difference_days"])
-            difference_days_list.append(row["difference_days"])
-            artistes.append(row)
-
-    # Calcul moyenne
-    moyenne = sum(difference_days_list) / len(difference_days_list)
-    min_val = min(difference_days_list)
-    max_val = max(difference_days_list)
-
-    def value_to_color(val):
-        pastel_min = 195  # plancher pastel
-
-        if val <= moyenne:
-            # ratio 0 → moyenne
-            ratio = (val - min_val) / (moyenne - min_val + 1e-6)
-            r = int(pastel_min + (255 - pastel_min) * ratio)  # 156 → 255
-            g = 255  # vert maximum
-        else:
-            # ratio moyenne → max
-            ratio = (val - moyenne) / (max_val - moyenne + 1e-6)
-            r = 255  # rouge maximum
-            g = int(pastel_min + (255 - pastel_min) * (1 - ratio))  # 255 → 156
-        b = pastel_min  # fixe pour un ton pastel doux
-
-        return f"rgb({r},{g},{b})"
-
-
-    for row in artistes:
-        row["color"] = value_to_color(row["difference_days"])
-
-    return render_template("famous.html", artistes=artistes)
-
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render fournit le port via cette variable
-    app.run(host="0.0.0.0", port=port)
-    #app.run(debug=True)
+    #port = int(os.environ.get("PORT", 5000))  # Render fournit le port via cette variable
+    #app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
