a48472bcc47bb1fb6a7730ea80c1e755985cc9ab
VILLEVY Geoffrey <gvillevy@macbookair-1.home>
2025-11-28 12:02:58 +0100

    update 28_11

--- app/app.py
+++ app/app.py
@@ -71,6 +71,6 @@ def pepites():
 
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))  # Render fournit le port via cette variable
-    #app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render fournit le port via cette variable
+    app.run(host="0.0.0.0", port=port)
+    #app.run(debug=True)
